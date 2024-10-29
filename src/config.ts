58fc0ee9b8a7c152a3cfc7036d569ab7a2b900c6
浪子 <jkjoy@live.cn>
2024-10-29 15:09:29 +0800

    11

--- src/config.ts
+++ src/config.ts
@@ -37,12 +37,12 @@ export const navBarConfig: NavBarConfig = {
   links: [
     LinkPreset.Home,
     LinkPreset.Archive,
-    LinkPreset.About,
     {
-      name: 'Memos',
+      name: '说说',
       url: '/memos',     // Internal links should not include the base path, as it is automatically added
-      external: true,                               // Show an external link icon and will open in a new tab
+      external: false,                               // Show an external link icon and will open in a new tab
     },
+    LinkPreset.About,
     {
       name: 'GitHub',
       url: 'https://github.com/jkjoy',     // Internal links should not include the base path, as it is automatically added
